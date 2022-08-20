a77c532b471a0c0e15a651f3cacdab27be4ab317
victorFloresDubon <vdubon@outlook.com>
2022-08-20 11:56:29 -0600

    Updates

--- src/components/inicio/estadios.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Estadios() {
-    return (
-        <div> Acá irán los estadios</div>
-    );
-}
--- src/components/inicio/partidos.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Partidos(){
-    return(
-        <div>Acá irán los partidos</div>
-    );
-}
